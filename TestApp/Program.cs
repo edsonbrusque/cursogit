b1c2ddb022852044f7226b57d4c389c614c875ee
edsonbrusque <edsonbrusque@gmail.com>
2019-01-28 10:54:20 -0200

    Doing the third change for the third time

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -9,6 +9,7 @@ namespace TestApp {
         static void Main(string[] args) {
             Console.WriteLine("This is just a test made on 2019-01-29 09:57 UTC-2");
             Console.WriteLine("And this change was made on 2019-01-29 10:16 UTC-2");
+            Console.WriteLine("Just another change. This time 2019-01-29 10:53 UTC-2");
             Console.ReadKey();
         }
     }

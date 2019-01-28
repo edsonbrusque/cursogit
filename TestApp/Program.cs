79036f2c96efcb14bb0bb3658859da1cdb2664ef
edsonbrusque <edsonbrusque@gmail.com>
2019-01-28 10:14:13 -0200

    Test project initial commit

---
+++ TestApp/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("This is just a test made on 2019-01-29 09:57 UTC-2");
+            Console.ReadKey();
+        }
+    }
+}

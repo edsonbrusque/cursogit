6a40f2c108a6ee60038d643212079fc1898ac3d3
edsonbrusque <edsonbrusque@gmail.com>
2019-01-28 12:20:25 -0200

    fourth test commit

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -8,8 +8,8 @@ namespace TestApp {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("This is just a test made on 2019-01-29 09:57 UTC-2");
-            Console.WriteLine("And this change was made on 2019-01-29 10:16 UTC-2");
             Console.WriteLine("Just another change. This time 2019-01-29 10:53 UTC-2");
+            Console.WriteLine("And line was added on 2019-01-29 10:16 UTC-2 but moved here on 12:19");
             Console.ReadKey();
         }
     }

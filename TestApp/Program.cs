317220dd6a1c62e0e09e63f21efed16c248939c9
edsonbrusque <edsonbrusque@gmail.com>
2019-01-28 10:18:12 -0200

    First commit after first change.

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -8,6 +8,7 @@ namespace TestApp {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("This is just a test made on 2019-01-29 09:57 UTC-2");
+            Console.WriteLine("And this change was made on 2019-01-29 10:16 UTC-2");
             Console.ReadKey();
         }
     }
